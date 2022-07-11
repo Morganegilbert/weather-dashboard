71005da6e428fbd3ee718d5090a98057a550114f
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-07-10 20:37:55 -0400

    pulled location cords from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,38 @@ var resultList = document.querySelector('results');
 var fetchButton = document.getElementById('fetch-button');
 var apiKey = 'ed329abd942d01f3c302c755d746f0d1';
 
-function getApi() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
-    var geolocatingURL = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}'
+async function getApi() {
+    var cityName = 'Raleigh';
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude={part}&appid={API key}';
+    var geolocatingURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=8&appid=' + apiKey;
     var fiveDayUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
+    reverseGeolocatingURL = 'http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid=' + apiKey;
+    console.log('This is location URL', geolocatingURL);
 
-    fetch(requestUrl)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        for (var i = 0; i < data.length; i++) {
-        var divItem = document.createElement('div');
-        divItem.textContent = data[i].html_url;
-        resultList.appendChild(divItem);
-        }
-    })
+    let locationData = await getLocationData(geolocatingURL);
+    var lat = locationData[0].lat;
+    var lon = locationData[0].lon;
+    console.log('location data', locationData[0].lat);
+    console.log('location lat', lat);
+    console.log('location lon', lon);
+
+
+    // let lat = await getLocationData(geolocatingURL);
+
 }
-fetchButton.addEventListener('click', getApi);
+
+async function getLocationData(URL) {
+    try {
+        let res = await fetch(URL);
+        console.log('res', res.json);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+    let lat = await fetch(URL);
+
+}
+
+// async function 
+
+fetchButton.addEventListener('click', getApi());
